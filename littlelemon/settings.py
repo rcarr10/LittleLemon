f91eb3955e76142217a547da949cb26382f8ba27
rcarr10 <rcarrillo1016@cloud.com>
2024-05-11 22:26:41 -0700

    mysql update

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'LittleLemon',  
+        'USER': 'root',  
+        'PASSWORD': 'Rene1016',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 
@@ -128,8 +135,3 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-Rest_Framework ={
-    'Default_Renderer_Classes':[
-        'rest'
-    ]
-}
